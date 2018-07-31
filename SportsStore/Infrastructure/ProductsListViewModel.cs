f8ad9f8d2619f08d18e058d7bf7c307029537a2e
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2018-07-31 11:50:30 -0700

    add pagination

---
+++ SportsStore/Infrastructure/ProductsListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
