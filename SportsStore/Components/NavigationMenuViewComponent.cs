e888063f3b6471a12c3486a8444b5a42d24be88c
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2018-07-31 22:15:28 -0700

    Add category filter

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IProductRepository repository;
+
+        public NavigationMenuViewComponent(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
